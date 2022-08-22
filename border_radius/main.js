08e5a10e0aedc5c2c87dd3d99ac9e749d3c79f0d
Gabriel <gabrielsantosbomjesus@gmail.com>
2022-08-22 12:45:25 -0300

    Add function

--- border_radius/main.js
+++ border_radius/main.js
@@ -1,24 +1,21 @@
 const radius = document.querySelector('#radius');
-const input = document.querySelector('#top_left');
-const input2 = document.querySelector('#top_right');
-const input3 = document.querySelector('#bottom_right');
-const input4 = document.querySelector('#bottom_left');
+const input_tl = document.querySelector('#top_left');
+const input_tr = document.querySelector('#top_right');
+const input_br = document.querySelector('#bottom_right');
+const input_bl = document.querySelector('#bottom_left');
 const btn = document.querySelector('#btn');
 
+function delimita(value){
+    if(value.value === ''){
+        value.value = 0
+    }  
+}
 
-btn.addEventListener('click', (e) => {
-    if(input.value === ''){
-        input.value = 0
-    }
-    if(input2.value === ''){
-        input2.value = 0
-    }
-    if(input3.value === ''){
-        input3.value = 0
-    }
-    if(input4.value === ''){
-        input4.value = 0
-    }
+btn.addEventListener('click', () => {
+    delimita(input_tl)
+    delimita(input_tr)
+    delimita(input_br)
+    delimita(input_bl)
 
-    radius.style.borderRadius = `${input.value} ${input2.value} ${input3.value} ${input4.value}`;
+    radius.style.borderRadius = `${input_tl.value}px ${input_tr.value}px ${input_br.value}px ${input_bl.value}px`;
 })
